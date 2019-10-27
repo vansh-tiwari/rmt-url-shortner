cccb526b0d4d8904242ed5cc0428d41938f9a14b
vansh-tiwari <root@localhost.localdomain>
2019-10-27 12:34:31 +0530

    Shortner continued

--- shortener/models.py
+++ shortener/models.py
@@ -1,11 +1,23 @@
+import random
+import string
 from django.db import models
 
 # Create your models here.
+def code_generator(size = 6, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 class RmtURL(models.Model):
     url = models.CharField(max_length=220,)
     shortcode = models.CharField(max_length = 15, unique=True )
     updated = models.DateTimeField(auto_now = True)
     timestamp = models.DateTimeField(auto_now_add = True)
 
+    def save(self, *args, **kwargs):
+        self.shortcode = code_generator()
+        super(RmtURL, self).save(*args, **kwargs)
+
     def __str__(self):
+        return str(self.url)
+
+    def __unicode__(self):
         return str(self.url)
